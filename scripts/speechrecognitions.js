b70802bff5fb2c77f41e97a63d9ee3c7ea126b21
Noam <noamoss@gmail.com>
2019-02-17 22:19:43 +0200

    add basic Hebrew speech recognition to the mobileapp

---
+++ scripts/speechrecognitions.js
@@ -0,0 +1,33 @@
+const talkButton = document.getElementById('talk');
+
+talkButton.addEventListener('click',  () => {
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.interimResults = true;
+recognition.lang="he";
+
+let p = document.createElement('p');
+const story = document.querySelector('#storyField');
+
+recognition.addEventListener('result', e=> {
+  const transcript = Array.from(e.results)
+  .map(result => result[0])
+  .map(result => result.transcript)
+  .join('');
+  console.log(transcript);
+
+  if(e.results[0].isFinal) {
+    story.value = transcript;
+  }
+
+  if(transcript.includes('שליחה')) {
+    console.log("שליחה");
+  }
+
+});
+
+recognition.addEventListener('end', recognition.stop);
+recognition.start()
+});
