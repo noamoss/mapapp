a08d99f26066d84f9bf0a7709b01cd67f9fd8b5a
Noam <noamoss@gmail.com>
2019-02-20 13:42:06 +0200

    fix code style

--- scripts/speechrecognitions.js
+++ scripts/speechrecognitions.js
@@ -1,49 +1,45 @@
-const talkButton = document.getElementById('talk');
-const speechIndicator = document.getElementById('speechIndication');
+const talkButton = document.getElementById('talk')
+const speechIndicator = document.getElementById('speechIndication')
 
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-const story = document.querySelector('#storyField');
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const recognition = new SpeechRecognition()
+const story = document.querySelector('#storyField')
 
-recognition.interimResults = true;
-recognition.lang="he";
+recognition.interimResults = true
+recognition.lang = 'he'
 
+var talk = true
 
-var talk=true;
+const changeTalkStatus = function () {
+  talk = !talk
 
-const changeTalkStatus = function() {
-  talk = !talk;
-
-  if (talk==true) {
-    speechIndicator.innerText = "דבר/י, אנחנו כותבים";
-    recognition.start();
-    recognition.addEventListener('result', e=> {
+  if (talk === true) {
+    speechIndicator.innerText = 'דבר/י, אנחנו כותבים'
+    recognition.start()
+    recognition.addEventListener('result', e => {
       const transcript = Array.from(e.results)
-      .map(result => result[0])
-      .map(result => result.transcript)
-      .join('');
-      console.log(transcript);
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+      console.log(transcript)
 
-      if(e.results[0].isFinal) {
-        story.value = transcript;
+      if (e.results[0].isFinal) {
+        story.value = transcript
       }
 
-      if(transcript.includes('שליחה')) {
-        console.log("שליחה");
+      if (transcript.includes('שליחה')) {
+        console.log('שליחה')
       }
+    })
 
-  });
-
-  recognition.addEventListener('end', () =>
-        {
-          recognition.stop();
-          speechIndicator.innerText = "";
-        })}
-
-  else {
-    speechIndicator.innerText = "";
-    recognition.stop();
+    recognition.addEventListener('end', () => {
+      recognition.stop()
+      speechIndicator.innerText = ''
+    })
+  } else {
+    speechIndicator.innerText = ''
+    recognition.stop()
   }
-};
+}
 
-document.addEventListener('click', changeTalkStatus);
+talkButton.addEventListener('click', changeTalkStatus)
